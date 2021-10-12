e72e3c7df51de320c740c921ec0bd77489febe62
Rafael <khachikyan38@gmail.com>
2021-10-12 23:44:00 +0300

    final project

--- js/script.js
+++ js/script.js
@@ -6,5 +6,14 @@ button.forEach((elem) => {
   elem.addEventListener("click", () => {
     box.classList.toggle("content__social-box--active");
     elem.classList.toggle("content__button--color");
+    expandAria(elem);
   });
 });
+
+const expandAria = function (elem) {
+  let ariaExpanded = elem.getAttribute("aria-expanded");
+
+  ariaExpanded === "true" ? (ariaExpanded = "false") : (ariaExpanded = "true");
+
+  elem.setAttribute("aria-expanded", ariaExpanded);
+};
